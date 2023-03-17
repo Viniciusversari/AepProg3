4e6c271a02fdd3d26e07e9d104c430e03cf97378
vinicius V <viniciusversarimarcusso@hotmail.com>
2023-03-17 18:07:03 -0300

    Commit

---
+++ src/controller/productController.ts
@@ -0,0 +1,30 @@
+import {writeFile,readFile} from 'fs/promises'
+import { Request, Response } from 'express'
+import productService from '../service/productService';
+
+class productController{
+
+    public async createProduct(req:Request,res: Response){
+        const product= productService.createProductList(req.body)
+        return res.status(201).send()
+    }
+
+
+    public async getProduct(req:Request,res:Response){
+        const product = await productService.getProducts()
+        return res.json(product)
+    }
+
+    public async getProduct_Stock(req:Request,res:Response){
+        const productList=await productService.getStock()    
+        return res.json(productList)
+    }
+
+    public async getTotalStock(req:Request,res:Response){
+        const stockValue=await productService.getStockValue() 
+
+        return res.json(stockValue)
+    }
+}
+
+export default new productController();
